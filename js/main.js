9ba01547d820428c8eb7065519ed87d9beb8abd6
Alex And <alexandr_works@ukr.net>
2017-09-10 17:35:31 +0300

    result

--- js/main.js
+++ js/main.js
@@ -14,72 +14,37 @@ $(document).ready(function (){
 
     var $first = $('.first img');
     var $second = $('.second img');
-    var min = 0;
+    var min = 0, num = 0;
     var max = source.length - 1;
 
     var num_first, num_second, currentPosFirst, currentPosSecond;
 
-    function getRandomValue () {
-        num_first = Math.floor(Math.random() * (max - min + 1) + min);
-        num_second = Math.floor(Math.random() * (max - min + 1) + min);
-
-        if (num_first == num_second) {
-            num_first = Math.floor(Math.random() * 5);
-            num_second = Math.floor(Math.random() * 5 + 5);
-        }
-
+    function getRandomValue() {
+        num_first = Math.floor(Math.random() * 5);
+        num_second = Math.floor(Math.random() * 5 + 5);
         return num_first, num_second;
     }
 
+    function toggler() {
+        num++;
+        return num;
+    }
     
 
     function setAttr() {
         getRandomValue();
-
-        currentPosFirst = parseInt($first.attr('src', source[num_first]).attr('src').replace(/[^\d.]/g, ''));
-        currentPosSecond = parseInt($second.attr('src', source[num_second]).attr('src').replace(/[^\d.]/g, ''));
-        
-
-        
-        return currentPosFirst, currentPosSecond;
-    }
-
-    function checkValue () {
-        setAttr();
-
-        if (currentPosFirst >= 5 && currentPosSecond <= 4) {
-            num_first = Math.floor(Math.random() * 5 + 5);
-            num_second = Math.floor(Math.random() * 5);
-            console.log(num_first, num_second);
-            return num_first, num_second;
+        toggler();
+        if (num % 2 != 0) {
+            $first.attr('src', source[num_first]);
+            $second.attr('src', source[num_second]);
+        } else {
+            $first.attr('src', source[num_second]);
+            $second.attr('src', source[num_first]);
         }
-
-        if (currentPosFirst <= 4 && currentPosSecond >= 5) {
-            num_first = Math.floor(Math.random() * 5 + 5);
-            num_second = Math.floor(Math.random() * 5 + 5);
-            console.log(num_first, num_second);
-            return num_first, num_second;
-        }
-
-        if (currentPosFirst >= 5 && currentPosSecond >= 5) {
-            num_first = Math.floor(Math.random() * 5);
-            num_second = Math.floor(Math.random() * 5 + 5);
-            console.log(num_first, num_second);
-            return num_first, num_second;
-        }
-
-        if (currentPosFirst <= 4 && currentPosSecond <= 4) {
-            num_first = Math.floor(Math.random() * 5);
-            num_second = Math.floor(Math.random() * 5);
-            console.log(num_first, num_second);
-            return num_first, num_second;
-        }
-
-        
     }
 
     setInterval(function() { 
-        checkValue();
+        setAttr();
     }, 1500);
 });
 
